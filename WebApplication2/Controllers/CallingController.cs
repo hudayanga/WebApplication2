79f6e9af43167e9fcba7b87b6ac26a12fac0f77f
Harsha Tennakoon <htennakoon@gmail.com>
2019-01-27 20:40:42 +0530

    enabled skype

---
+++ WebApplication2/Controllers/CallingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Bot.Builder.Calling;
+using Microsoft.Bot.Connector;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApplication2.Voice;
+
+namespace WebApplication2.Controllers
+{
+    public class CallingController : ApiController
+    {
+        public CallingController() : base()
+        {
+            CallingConversation.RegisterCallingBot(callingBotService => new DemoCallingBot(callingBotService));
+        }
+
+        [Route("callback")]
+        public async Task<HttpResponseMessage> ProcessCallingEventAsync()
+        {
+            return await CallingConversation.SendAsync(this.Request, CallRequestType.CallingEvent);
+        }
+
+        [Route("call")]
+        public async Task<HttpResponseMessage> ProcessIncomingCallAsync()
+        {
+            return await CallingConversation.SendAsync(this.Request, CallRequestType.IncomingCall);
+        }
+    }
+}
